fdeda5458a5b881188838b18979faecb12ebafaf
OleksandrLukomskyi <121460896+OleksandrLukomskyi@users.noreply.github.com>
2024-04-16 12:24:01 +0200

    add

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,7 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare<T extends keyof AllType, K extends keyof AllType>(top: Pick<AllType, T>, bottom: Pick<AllType, K>): AllType {
   return {
     name: top.name,
     color: top.color,
